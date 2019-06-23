914930fc857f67e81e2d7afbb42a91e9f62c3ace
romain sanson <romainsanson.gm@gmail.com>
2019-06-23 02:15:40 +0200

    search bar component done

---
+++ src/components/gif.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Gif extends Component {
+  render () {
+    return <img src="https://media2.giphy.com/media/gj0xbBpmWbJXvynIeF/giphy.webp" alt="" className="gif"/>
+  }
+}
+
+export default Gif
