77ddee3c5ac4605e4b9f68e8360dc817f3c900a3
romain sanson <romainsanson.gm@gmail.com>
2019-06-23 03:49:43 +0200

    clean

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import App from './components/app'
 
 import '../assets/stylesheets/application.scss';
 
-
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<App />, root);
